39bbe1d0566c36e73388befd92edfb944d22c1a6
Hariyanto Wibowo <hariwib@darmajaya.ac.id>
2023-03-22 11:08:31 +0700

    update file controller

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("aplikasi berjalan");
+    response.ok("aplikasi Rest API berjalan",res);
 };
