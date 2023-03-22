17fccb88100e56635350c22eb8273dd383364e03
Hariyanto Wibowo <hariwib@darmajaya.ac.id>
2023-03-22 11:28:12 +0700

    update controller router dan tutor

--- controller.js
+++ controller.js
@@ -12,9 +12,23 @@ exports.index = function(req,res){
 exports.tampilsemuamahasiswa = function(req,res){
     connection.query('select * from mahasiswa',function(error,rows,fileds){
         if(error){
-            connection.log(error);
+            console.log(error);
         }else{
             response.ok(rows,res)
         }
     });
 };
+
+
+//menampilkan data mahasiswa berdasarkan idnya
+exports.tampilberdasarkanid = function(req,res){
+    let id = req.params.id;
+    connection.query('select * from mahasiswa where id = ?',[id],
+        function(error, rows, fileds){
+            if(error){
+                console.log(error);
+            }else{
+                response.ok(rows,res);
+            }
+        });
+}; 
