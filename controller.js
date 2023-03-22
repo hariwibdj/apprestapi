590902a31e4c8af5d06e921f22832bc65daa6233
Hariyanto Wibowo <hariwib@darmajaya.ac.id>
2023-03-22 11:17:47 +0700

    update file controller dan routes

--- controller.js
+++ controller.js
@@ -6,3 +6,15 @@ var connection = require('./koneksi');
 exports.index = function(req,res){
     response.ok("aplikasi Rest API berjalan",res);
 };
+
+//menampilkan data mahasiswa
+
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('select * from mahasiswa',function(error,rows,fileds){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows,res)
+        }
+    });
+};
