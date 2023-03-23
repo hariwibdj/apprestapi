2a8886968f67f5b0f91d502622e0fdf2b8b0b57a
Hariyanto Wibowo <hariwib@darmajaya.ac.id>
2023-03-23 15:09:00 +0700

    update server.js

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express   = require('express');
 const bodyParse = require('body-parser');
+
+var morgan = require('morgan');
 const app = express();
 
 //parse application/json
 app.use(bodyParse.urlencoded({extended:true})); //bourl
 app.use(bodyParse.json()); //bojson
+app.use(morgan('dev'));
+
 
 //panggil routes
 var routes  = require('./routes');
 routes(app);
 
+//daftarkan menu routes dari index
+app.use('/auth',require('./middleware'));
+
+
 app.listen(3000, () => {
-    console.log(`Server started on port`);
+    console.log('Server started on port 3000');
 });
 
