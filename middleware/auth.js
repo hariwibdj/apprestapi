c4ca711210068ae33bd5ac12c16885df2c9c6eed
Hariyanto Wibowo <hariwib@darmajaya.ac.id>
2023-03-23 16:19:04 +0700

    update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 var connection = require('../koneksi');
 var mysql = require('mysql');
-var md5 = require('MD5');
+var md5 = require('md5');
 var response = require('../res');
 var jwt = require('jsonwebtoken');
 var config = require('../config/secret');
@@ -9,35 +9,35 @@ var ip = require('ip');
 //controller untuk register
 exports.registrasi = function(req,res){
     var post = {
-        username : req.body.username,
-        email : req.body.email,
-        password : md5(reg.body.password),
-        role : reg.body.role,
-        tanggaldaftar : new Date()
-    }
+        username: req.body.username,
+        email: req.body.email,
+        password: md5(req.body.password),
+        role: req.body.role,
+        tanggaldaftar: new Date()
+   }
 
-    var query = "SELECT email FROM ?? WHERE ??";
-    var table = ["user","email",post.email];
+   var query = "SELECT email FROM ?? WHERE ??=?";
+   var table = ["user", "email", post.email];
 
-    query = mysql.format(query,table);
+   query = mysql.format(query, table);
 
-    connection.query(query,function(error,rows){
-        if(error){
-            console.log(error);
-        }else{
-            if(rows.length == 0){
-                var query = "INSERT INTO ?? SET ?";
-                var table = ["user"];
-                query = mysql.format(query,table);
-                connection.query(query,post,function(error,rows){
-                    if(error){
+   connection.query(query, function (error, rows) {
+    if (error) {
+         console.log(error);
+    } else {
+         if (rows.length == 0) {
+              var query = "INSERT INTO ?? SET ?";
+              var table = ["user"];
+              query = mysql.format(query, table);
+              connection.query(query, post, function (error, rows) {
+                   if (error) {
                         console.log(error);
                     }else{
                         response.ok("Berhasil menambahkan data user baru",res);
                     }
                 });
             }else{
-                response.ok("email sudah terdaftar");
+                response.ok("email sudah terdaftar",res);
             }
         }
     })
